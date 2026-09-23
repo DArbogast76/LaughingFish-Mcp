445312b80bdbedae254b391175113cd5845fc294
David Arbogast <DArbogast@EzCOITracker.com>
2026-09-23 03:07:37 -0500

    bug fix

--- Location/AzureMapsLocationResolver.cs
+++ Location/AzureMapsLocationResolver.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Net.Http.Headers;
 using System.Text.Json;
 using Azure.Core;
@@ -226,8 +227,7 @@ public sealed class AzureMapsLocationResolver : ILocationResolver
             ExcludeInteractiveBrowserCredential = true,
             ExcludeVisualStudioCredential = true,
             ExcludeVisualStudioCodeCredential = true,
-            ExcludeAzurePowerShellCredential = true,
-            ExcludeSharedTokenCacheCredential = true
+            ExcludeAzurePowerShellCredential = true
         });
     }
 }
